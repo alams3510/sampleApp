fad95bb8838f4f95ef669d4786110c4457a8722a
alams3510 <alams3510@gmail.com>
2022-12-21 22:59:38 -0800

    updated second commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,77 +1,58 @@
 import React from 'react'
 
+
+
 function Services() {
-  return (
-    <div className='container'>
+  const Mockdata=[
+    {title:"special title treatment",
+      desc:"With supporting text below as a natural lead-in to additional content.",
+      icon:"fa-brands fa-slack"
+        },
+        {title:"special title treatment",
+      desc:"With supporting text below as a natural lead-in to additional content.",
+      icon:"fa fa-address-book-o"
+        },
         
+        {title:"special title treatment",
+        desc:"With supporting text below as a natural lead-in to additional content.",
+        icon:"fa fa-snowflake-o"
+          },
+          {title:"special title treatment",
+          desc:"With supporting text below as a natural lead-in to additional content.",
+          icon:"fa fa-user-circle"
+            },
+            {title:"special title treatment",
+            desc:"With supporting text below as a natural lead-in to additional content.",
+            icon:"fa fa-meetup"
+              },
+              {title:"special title treatment",
+              desc:"With supporting text below as a natural lead-in to additional content.",
+             icon: "fa fa-handshake-o"
+                },
+  ]
+  return (
+    <div className='container'>      
         <h1 className='text-center my-2'>Our <span className='text-primary'>Services</span></h1>
         <hr className='w-25 m-auto mb-4'/>
         
-<div class="row">
-  <div class="col-sm-12 col-md-6 col-lg-4 p-5">
-    <div class="card">
-      <div class="card-body">
-        <i className="fa fa-users"></i>
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+  <div class="row">
+  {
+    Mockdata.map((item,i)=>{
+      return <div class="col-sm-12 col-md-6 col-lg-4 p-3" key={i}>
+      <div class="card">
+        <div class="card-body">
+        <i className={item.icon}></i>
+          <h5 className="card-title">{item.title}</h5>
+          <p className="card-text">{item.desc}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="col-sm-12 col-md-6 col-lg-4 my-2 my-md-0 p-5">
-    <div class="card text-white bg-primary">
-      <div class="card-body">
-      <i class="fa-brands fa-dropbox bg-white text-white"></i>
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn bg-white text-dark">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-
-  <div class="col-sm-12 col-md-6 col-lg-4 p-5">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
+    })
+  }
+  
 </div>
-
-
-<div class="row mt-2">
-  <div class="col-sm-12 col-md-6 col-lg-4 p-5">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-12 col-md-6 col-lg-4 my-2 my-md-0 p-5">
-    <div class="card bg-primary text-white">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn bg-white text-dark">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-
-  <div class="col-sm-12 col-md-6 col-lg-4 p-5">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
 </div>
-    </div>
   )
 }
 
